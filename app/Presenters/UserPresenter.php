687300a5dd2d7e65a37fdddd3d8f17ea1ffc7fa6
FoUkR <mich.slovak@gmail.com>
2020-01-22 19:21:13 +0100

    Edit readme.md & edit default latte at Homepage template

---
+++ app/Presenters/UserPresenter.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Presenters;
+
+use Nette;
+
+
+final class HomepagePresenter extends Nette\Application\UI\Presenter
+{
+    /** @var Nette\Database\Context */
+    private $database;
+
+    public function __construct(Nette\Database\Context $database)
+    {
+        $this->database = $database;
+    }
+
+    public function renderDefault(): void
+    {
+        $this->template->posts = $this->database->table('posts')
+            ->order('created_at DESC')
+            ->limit(5);
+    }
+
+}
