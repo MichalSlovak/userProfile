af4312cd637838d027cea8175e0559a281d40394
FoUkR <mich.slovak@gmail.com>
2020-01-28 14:58:25 +0100

    created User Presenter

--- app/Presenters/UserPresenter.php
+++ app/Presenters/UserPresenter.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace App\Presenters;
 
 use Nette;
-
+use Nette\Security\User;
 
 final class UserPresenter extends Nette\Application\UI\Presenter
 {
@@ -15,21 +15,28 @@ final class UserPresenter extends Nette\Application\UI\Presenter
     /** @var Nette\Security\User */
     public $user;
 
+
     public function __construct(Nette\Database\Context $database)
     {
         $this->database = $database;
-    }
 
-    public function renderDefault(): void
-    {
-        $this->template->posts = $this->database->table('posts')
-            ->order('created_at DESC')
-            ->limit(5);
+
     }
-/*
-    public function renderDefault($user, $username = 1, $password = 1 ): void
-    {
-        $user->login($username, $password);
-    }
-*/
+    /*
+            public function renderDefault(): void
+            {
+                $this->template->posts = $this->database->table('posts')
+                    ->order('created_at DESC')
+                    ->limit(5);
+            }
+         */
+             public function renderDefault($username = 1, $password = 1): void
+             {
+                 $user = $this->getUser();
+                // $user->login($username, $password);
+
+             }
+
+
+
 }
